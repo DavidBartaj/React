064bbab1d8389432d239534f37a831d8a60fea82
DavidBartaj <44649362+DavidBartaj@users.noreply.github.com>
2019-03-16 16:24:18 +0200

    Mission HTML+CSS+JS complete
    Проблеми тільки з наведенням на картинки в розділі meet our team. Не може по-людський зробити аби вискакував надпис під конкретним фото, при наведенні напис з'я вляється на всіх фото.

--- Final_project/script.js
+++ Final_project/script.js
@@ -46,4 +46,73 @@ $(document).ready(function () {
             color: `#ff2d2d`
         })
     });
+    $(`.featureProductsNav li`).mouseover(function () {
+        $(this).css({
+            cursor: `pointer`,
+            color: `#ff2d2d`,
+
+        })
+    });
+    $('.featureProductsNav li').mouseout(function () {
+        $(this).css({
+            color: ``
+        })
+    });
+
+
+    // Не перебирає фото
+
+    $(`.teamPhotoWrapper div`).mouseover(function () {
+        $(this).each(function (i, elem) {
+            console.log($(this) + [i]);
+            $(`.accountNameOnPhoto`).css({
+                display: `flex`,
+            });
+        });
+        $(this).css({
+            cursor: `pointer`
+        });
+    });
+
+
+    $(`.teamPhotoWrapper div`).mouseout(function () {
+        $(`.accountNameOnPhoto`).css({
+            display: `none`
+        })
+    });
+
+    // end
+
+    $(`.subscribeButton`).mouseover(function () {
+        $(this).css({
+            cursor: `pointer`,
+            background: `rgba(0, 0, 0, 0)`,
+            border: `3px solid #ff2d2d`
+
+        })
+    });
+    $(`.subscribeButton`).mouseout(function () {
+        $(this).css({
+            background: ``,
+            border: ``
+
+        })
+    });
+    $(`.sendMessageButt`).mouseover(function () {
+        $(this).css({
+            background: `#ffffff`,
+            cursor: `pointer`,
+            color: `#ff2d2d`,
+            border: `3px solid #ff2d2d`
+
+        })
+    });
+    $('.sendMessageButt').mouseout(function () {
+        $(this).css({
+            background: `#ff2d2d`,
+            color: `#ffffff`,
+            border: `none`
+        })
+    });
+
 });
